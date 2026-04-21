fad971a48e4b5ed478a057c03cb60d9adb7d397e
Reshma Bidikar <85998496+reshmabidikar@users.noreply.github.com>
2026-04-21 11:42:38 +0530

    Update tests

--- profiles/killbill/src/test/java/org/killbill/billing/jaxrs/TestCredit.java
+++ profiles/killbill/src/test/java/org/killbill/billing/jaxrs/TestCredit.java
@@ -22,6 +22,7 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
 import org.killbill.billing.client.KillBillClientException;
 import org.killbill.billing.client.model.InvoiceItems;
 import org.killbill.billing.client.model.gen.Account;
@@ -62,11 +63,11 @@ public class TestCredit extends TestJaxrsBase {
 
         final InvoiceItems credits = new InvoiceItems();
         credits.add(credit);
-        InvoiceItems createdCredits = creditApi.createCredits(credits, false, NULL_PLUGIN_PROPERTIES, requestOptions);
+        InvoiceItems createdCredits = creditApi.createCredits(credits, clock.getUTCToday(), false, NULL_PLUGIN_PROPERTIES, requestOptions);
 
         final UUID invoiceId = createdCredits.get(0).getInvoiceId();
         credit.setInvoiceId(invoiceId);
-        createdCredits = creditApi.createCredits(credits, false, NULL_PLUGIN_PROPERTIES, requestOptions);
+        createdCredits = creditApi.createCredits(credits, clock.getUTCToday(), false, NULL_PLUGIN_PROPERTIES, requestOptions);
 
         // We can't just compare the object via .equals() due e.g. to the invoice id
         assertEquals(createdCredits.get(0).getAccountId(), accountJson.getAccountId());
@@ -78,6 +79,34 @@ public class TestCredit extends TestJaxrsBase {
         assertEquals(createdCredits.get(0).getRate().compareTo(BigDecimal.TEN), 0);
         assertEquals(createdCredits.get(0).getItemDetails().compareTo("itemDetails"), 0);
     }
+
+    @Test(groups = "slow", description = "Can add a credit with a different date to an existing invoice")
+    public void testAddCreditWithDifferentDateToInvoice() throws Exception {
+        final DateTime effectiveDate = clock.getUTCNow();
+        final BigDecimal creditAmount = BigDecimal.ONE;
+        final InvoiceItem credit = new InvoiceItem();
+        credit.setAccountId(accountJson.getAccountId());
+        credit.setAmount(creditAmount);
+        credit.setDescription("description");
+        credit.setQuantity(new BigDecimal("5"));
+        credit.setRate(BigDecimal.TEN);
+        credit.setItemDetails("itemDetails");
+
+        final InvoiceItems credits = new InvoiceItems();
+        credits.add(credit);
+        final LocalDate creditEffectiveDate = clock.getUTCToday().minusMonths(1);
+        InvoiceItems createdCredits = creditApi.createCredits(credits, creditEffectiveDate, false, NULL_PLUGIN_PROPERTIES, requestOptions);
+
+        // We can't just compare the object via .equals() due e.g. to the invoice id
+        assertEquals(createdCredits.get(0).getAccountId(), accountJson.getAccountId());
+       assertEquals(createdCredits.get(0).getAmount().compareTo(creditAmount), 0);
+        assertEquals(createdCredits.get(0).getStartDate().compareTo(creditEffectiveDate), 0);
+        assertEquals(createdCredits.get(0).getDescription().compareTo("description"), 0);
+        assertEquals(createdCredits.get(0).getQuantity().compareTo(new BigDecimal("5")), 0);
+        assertEquals(createdCredits.get(0).getRate().compareTo(BigDecimal.TEN), 0);
+        assertEquals(createdCredits.get(0).getItemDetails().compareTo("itemDetails"), 0);
+    }
+
 
     @Test(groups = "slow", description = "Can add a credit to an existing account",
             expectedExceptions = KillBillClientException.class, expectedExceptionsMessageRegExp = ".*it is already in COMMITTED status")
@@ -92,7 +121,7 @@ public class TestCredit extends TestJaxrsBase {
         final InvoiceItems credits = new InvoiceItems();
         credits.add(credit);
 
-        final InvoiceItems objFromJson = creditApi.createCredits(credits, true, NULL_PLUGIN_PROPERTIES, requestOptions);
+        final InvoiceItems objFromJson = creditApi.createCredits(credits, clock.getUTCToday(), true, NULL_PLUGIN_PROPERTIES, requestOptions);
         Assert.assertTrue(objFromJson.get(0).getAmount().compareTo(creditAmount) == 0);
     }
 
@@ -106,7 +135,7 @@ public class TestCredit extends TestJaxrsBase {
         credits.add(credit);
 
         // Try to create the credit
-        final InvoiceItems result = creditApi.createCredits(credits, true, NULL_PLUGIN_PROPERTIES, requestOptions);
+        final InvoiceItems result = creditApi.createCredits(credits, clock.getUTCToday(), true, NULL_PLUGIN_PROPERTIES, requestOptions);
         assertEquals(result.size(), 0);
     }
 
@@ -120,7 +149,7 @@ public class TestCredit extends TestJaxrsBase {
 
         // Try to create the credit
         try {
-            creditApi.createCredits(credits, true, NULL_PLUGIN_PROPERTIES, requestOptions);
+            creditApi.createCredits(credits, clock.getUTCToday(), true, NULL_PLUGIN_PROPERTIES, requestOptions);
             fail();
         } catch (final KillBillClientException e) {
         }
